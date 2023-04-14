7af5c90ae16892125e711e0352f2731543dc4dc9
Angelena Nunez <angelenacorral@gmail.com>
2023-04-14 10:46:31 -0600

    JS started, html script getValues updated

--- js/site.js
+++ js/site.js
@@ -1,7 +1,25 @@
-function displayMessage() {
-    // represents value in text box
-    let msg = document.getElementById('message').value;
-    // alert(msg);
+// App Controller / Entry Point
+function getValues() {
+    // get the users input
+    // decide what to do with it
+    
+    
+}
+
+// Business Logic
+function reverseString(message) {
+    // take a string, return it in reverse
+    
+    
+}
+// View Function
+function displayString(reverseMessage) {
+    // show the string on the page / assign text input to reverseMessage
+    document.getElementById('msg').textContent = reverseMessage;
+    // you want to remove the class d-none to get the alert to show / display
+    document.getElementById('alert').classList.remove('d-none')
+}
+
     Swal.fire(
         {
             backdrop: false,
@@ -9,4 +27,3 @@ function displayMessage() {
             text: msg
         }
     );
-}
