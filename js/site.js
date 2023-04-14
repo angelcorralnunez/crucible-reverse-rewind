657de1c66944b7f28e81e7fc0b2c10d67a98227b
Angelena Nunez <angelenacorral@gmail.com>
2023-04-14 11:42:14 -0600

    JS functions in progress

--- js/site.js
+++ js/site.js
@@ -2,28 +2,37 @@
 function getValues() {
     // get the users input
     // decide what to do with it
+    let userInput = document.getElementById('message').value;
     
-    
+    let reversedInput = reverseString(userInput)
+
+    displayString('reversedInput');
 }
 
 // Business Logic
 function reverseString(message) {
     // take a string, return it in reverse
-    
-    
+    let reversedMessage = ''
+
+    //  reverse the string
+    for (let index = message.length - 1; index >= 0; index - 1){
+        reversedMessage += message[index];
+    }
+
+    return reversedMessage;
 }
 // View Function
-function displayString(reverseMessage) {
+function displayString(reversedMessage) {
     // show the string on the page / assign text input to reverseMessage
     document.getElementById('msg').textContent = reverseMessage;
     // you want to remove the class d-none to get the alert to show / display
     document.getElementById('alert').classList.remove('d-none')
 }
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'Reverse Rewind',
-            text: msg
-        }
-    );
+    // Swal.fire(
+    //     {
+    //         backdrop: false,
+    //         title: 'Reverse Rewind',
+    //         text: msg
+    //     }
+    // );
